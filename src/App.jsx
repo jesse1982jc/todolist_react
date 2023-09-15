bd25325426fc11e83994c270f81c73cee093ff82
Jesse <ya710728@gmail.com>
2023-09-16 00:01:01 +0800

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import CssBaseline from "@mui/material/CssBaseline";
 import "./App.css";
 import TodoList from "./TodoList";
